ef6d1dcb65f4a687b3d6130def14dc8db7ee8908
jyothithomas <jyothithomas.05@gmail.com>
2020-06-25 02:20:41 -0400

    fetch data

--- todo_Jyothi_C0775696/ViewController.swift
+++ todo_Jyothi_C0775696/ViewController.swift
@@ -7,25 +7,49 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
 
     @IBOutlet weak var table: UITableView!
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var result:[MyList] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         table.delegate = self
         table.dataSource = self
         // Do any additional setup after loading the view.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        fetch()
+        table.reloadData()
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return result.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = table.dequeueReusableCell(withIdentifier: "id", for: indexPath) as! pCellTableViewCell
+        let rslt = result[indexPath.row]
+        cell.task.text = rslt.task
+        cell.priority.text = rslt.priority
+        cell.priority.textColor = rslt.color
         return cell
     }
-    
+    func fetch()
+    {
+        let request = NSFetchRequest<MyList>(entityName: "MyList")
+        do{
+            result = try context.fetch(request)
+        }
+        catch{
+            print(error)
+        }
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140    }
+
 
 
 }
