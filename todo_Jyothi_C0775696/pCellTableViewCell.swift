4b9931386f7b82f58e07c62b92e8366976fd3b8e
jyothithomas <jyothithomas.05@gmail.com>
2020-06-25 01:08:44 -0400

    added picker view and tablecellvc

---
+++ todo_Jyothi_C0775696/pCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  pCellTableViewCell.swift
+//  todo_Jyothi_C0775696
+//
+//  Created by user176475 on 6/25/20.
+//  Copyright © 2020 user176475. All rights reserved.
+//
+
+import UIKit
+
+class pCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
