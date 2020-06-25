ac995d4878c15f126aefc31f69495a0759677b0c
jyothithomas <jyothithomas.05@gmail.com>
2020-06-24 23:19:16 -0400

    created core data and manual model

---
+++ todo_Jyothi_C0775696/model.swift
@@ -0,0 +1,14 @@
+//
+//  model.swift
+//  todo_Jyothi_C0775696
+//
+//  Created by user176475 on 6/24/20.
+//  Copyright © 2020 user176475. All rights reserved.
+//
+
+import Foundation
+import CoreData
+class MyList:NSManagedObject
+{
+    
+}
